5e0f2f616a604b653d96ebae12ffe52b9aaf70b6
Eugenio-Lopes <128297188+Eugenio-Lopes@users.noreply.github.com>
2023-07-23 15:21:09 -0300

    Add all files

---
+++ pages/consultarprotocolo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+import Base from "@layouts/Baseof";
+import theme from "@config/theme.json";
+
+const ConsultarProtocolo = () => {
+  return (
+  <Base>
+  <div><h2>Consulta de processos administrativos</h2></div>
+  </Base>
+  )
+};
+
+export default ConsultarProtocolo;
